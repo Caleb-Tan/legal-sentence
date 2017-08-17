097d82c6cc8b4db02298410560c2a53d96b53513
Caleb-Tan <calebtan23@gmail.com>
2017-08-17 09:44:11 -0700

    blah

--- LegalSentence/src/LegalSentence.java
+++ LegalSentence/src/LegalSentence.java
@@ -4,21 +4,28 @@ public class LegalSentence {
 	/* this method parses the string into an array of the words. It then begins going through 
 	 * each string value and checking it with cases that are in the other method. */
 	
-	public Boolean check(String sentence) {
+	public boolean check(String sentence) {
 		String[] splitted = sentence.split("\\s+"); //splits the string into an array based on spaces
 		String prevValue = "start"; 				// beginning token
-		for (int i = 0; i < splitted.length; i++){ 
+		for (int i = 0; i < splitted.length; i++) {
 			String[] nextValue = possibleTokens(prevValue); //calls the possibleTokens method and gets the return value
-			if (Arrays.asList(nextValue).contains(splitted[i])){	// if statement checks to see if the next value is one of the possible tokens, if not, returns false, if yes, continues to parse the string
+			if (Arrays.asList(nextValue).contains(splitted[i])) {    // if statement checks to see if the next value is one of the possible tokens, if not, returns false, if yes, continues to parse the string
 				prevValue = splitted[i];
 			} else {
-				System.out.println("Not a legal sentence.");	
+				System.out.println("Not a legal sentence.");
 				return false;
 			}
 
 		}
-		System.out.println("Is a legal sentence");
-		return true;
+		// this final if statement checks to see if the last value is p or q
+		if (splitted[splitted.length-1].equals("p") | splitted[splitted.length-1].equals("q")){
+			System.out.println("Is a legal sentence.");
+			return true;
+		}
+		else {
+			System.out.println("Not a legal sentence.");
+			return false;
+		}
 	}
 	
 	
@@ -29,17 +36,17 @@ public class LegalSentence {
 				String[] a = {"p","q","~"};
 				return a;
 			case "~":
-				String[] b = {"p", "q", "~"};
+				String[] b = {"p", "q", "~", "&", "|", "=>", "<=>",};
 				return b;
 			case "p":
 			case "q":
-				String[] c = {"&", "|", "=>", "<=>"};
+				String[] c = {"&", "|", "=>", "<=>", "~"};
 				return c;
 			case "&":
 			case "|":
 			case "=>":
 			case "<=>":
-				String[] d = {"p", "q"};
+				String[] d = {"p", "q", "~"};
 				return d;
 			default:
 				String[] e = {};
